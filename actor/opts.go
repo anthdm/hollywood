57dae4a605077f6c6b304bc039489e59850f2a52
anthdm <cryptoanthdm@gmail.com>
2023-04-21 11:16:57 +0200

    Added the options to configure the restart delay

--- actor/opts.go
+++ actor/opts.go
@@ -1,21 +1,26 @@
 package actor
 
+import "time"
+
 const (
 	defaultInboxSize   = 1024
 	defaultMaxRestarts = 3
 )
+
+var defaultRestartDelay = 500 * time.Millisecond
 
 type ReceiveFunc = func(*Context)
 
 type MiddlewareFunc = func(ReceiveFunc) ReceiveFunc
 
 type Opts struct {
-	Producer    Producer
-	Name        string
-	Tags        []string
-	MaxRestarts int32
-	InboxSize   int
-	Middleware  []MiddlewareFunc
+	Producer     Producer
+	Name         string
+	Tags         []string
+	MaxRestarts  int32
+	RestartDelay time.Duration
+	InboxSize    int
+	Middleware   []MiddlewareFunc
 }
 
 type OptFunc func(*Opts)
@@ -23,16 +28,23 @@ type OptFunc func(*Opts)
 // DefaultOpts returns default options from the given Producer.
 func DefaultOpts(p Producer) Opts {
 	return Opts{
-		Producer:    p,
-		MaxRestarts: defaultMaxRestarts,
-		InboxSize:   defaultInboxSize,
-		Middleware:  []MiddlewareFunc{},
+		Producer:     p,
+		MaxRestarts:  defaultMaxRestarts,
+		InboxSize:    defaultInboxSize,
+		RestartDelay: defaultRestartDelay,
+		Middleware:   []MiddlewareFunc{},
 	}
 }
 
 func WithMiddleware(mw ...MiddlewareFunc) OptFunc {
 	return func(opts *Opts) {
 		opts.Middleware = append(opts.Middleware, mw...)
+	}
+}
+
+func WithRestartDelay(d time.Duration) OptFunc {
+	return func(opts *Opts) {
+		opts.RestartDelay = d
 	}
 }
 
