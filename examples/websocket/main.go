c6495ef33ea0a2613c3219ccfc3c96e9289a5a62
Carlos Camilo <carloscamilocontato@gmail.com>
2023-08-13 11:38:07 -0300

    fix: lower case structs

--- examples/websocket/main.go
+++ examples/websocket/main.go
@@ -10,39 +10,39 @@ import (
 	"github.com/anthdm/hollywood/actor"
 )
 
-type Server struct {
+type server struct {
 	ctx      *actor.Context
 	sessions map[string]*actor.PID
 }
 
-func NewServer() actor.Receiver {
-	return &Server{
+func newServer() actor.Receiver {
+	return &server{
 		sessions: make(map[string]*actor.PID),
 	}
 }
 
-func (s *Server) Receive(ctx *actor.Context) {
+func (s *server) Receive(ctx *actor.Context) {
 	switch msg := ctx.Message().(type) {
 	case actor.Started:
 		log.Println("Server started on port 8080")
 		s.serve()
 		s.ctx = ctx
 		_ = msg
-	case Message:
+	case message:
 		s.broadcast(ctx.Sender(), msg)
 	default:
 		fmt.Printf("Server received %v\n", msg)
 	}
 }
 
-func (s *Server) serve() {
+func (s *server) serve() {
 	go func() {
 		http.HandleFunc("/ws", s.handleWebsocket)
 		http.ListenAndServe(":8080", nil)
 	}()
 }
 
-func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
+func (s *server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("New connection")
 	upgrader := websocket.Upgrader{}
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -52,12 +52,12 @@ func (s *Server) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 	}
 
 	username := r.URL.Query().Get("username")
-	pid := s.ctx.SpawnChild(NewUser(username, conn, s.ctx.PID()), username)
+	pid := s.ctx.SpawnChild(newUser(username, conn, s.ctx.PID()), username)
 
 	s.sessions[pid.GetID()] = pid
 }
 
-func (s *Server) broadcast(sender *actor.PID, msg Message) {
+func (s *server) broadcast(sender *actor.PID, msg message) {
 	for _, pid := range s.sessions {
 		if !pid.Equals(sender) {
 			s.ctx.Send(pid, msg)
@@ -65,21 +65,21 @@ func (s *Server) broadcast(sender *actor.PID, msg Message) {
 	}
 }
 
-type Message struct {
+type message struct {
 	Content string `json:"content"`
 	Owner   string `json:"owner"`
 }
 
-type User struct {
+type user struct {
 	conn      *websocket.Conn
 	ctx       *actor.Context
 	serverPid *actor.PID
 	Name      string
 }
 
-func NewUser(name string, conn *websocket.Conn, serverPid *actor.PID) actor.Producer {
+func newUser(name string, conn *websocket.Conn, serverPid *actor.PID) actor.Producer {
 	return func() actor.Receiver {
-		return &User{
+		return &user{
 			Name:      name,
 			conn:      conn,
 			serverPid: serverPid,
@@ -87,12 +87,12 @@ func NewUser(name string, conn *websocket.Conn, serverPid *actor.PID) actor.Prod
 	}
 }
 
-func (u *User) Receive(ctx *actor.Context) {
+func (u *user) Receive(ctx *actor.Context) {
 	switch msg := ctx.Message().(type) {
 	case actor.Started:
 		u.ctx = ctx
 		go u.listen()
-	case Message:
+	case message:
 		u.send(&msg)
 	case actor.Stopped:
 		_ = msg
@@ -102,8 +102,8 @@ func (u *User) Receive(ctx *actor.Context) {
 	}
 }
 
-func (u *User) listen() {
-	var msg Message
+func (u *user) listen() {
+	var msg message
 	for {
 		if err := u.conn.ReadJSON(&msg); err != nil {
 			fmt.Printf("Error reading message: %v\n", err)
@@ -116,7 +116,7 @@ func (u *User) listen() {
 	}
 }
 
-func (u *User) handleMessage(msg Message) {
+func (u *user) handleMessage(msg message) {
 	switch msg.Content {
 	case "exit": // Send exit message to stop the actor and close the websocket connection
 		u.ctx.Engine().Poison(u.ctx.PID())
@@ -126,7 +126,7 @@ func (u *User) handleMessage(msg Message) {
 	}
 }
 
-func (u *User) send(msg *Message) {
+func (u *user) send(msg *message) {
 	if err := u.conn.WriteJSON(msg); err != nil {
 		fmt.Printf("Error writing message: %v\n", err)
 		return
@@ -135,7 +135,7 @@ func (u *User) send(msg *Message) {
 
 func main() {
 	engine := actor.NewEngine()
-	engine.Spawn(NewServer, "server")
+	engine.Spawn(newServer, "server")
 
 	select {}
 }
