57c621d29bc21689be48d524e0f7143593608497
Anthony De Meulemeester <cryptoanthdm@gmail.com>
2023-08-14 10:49:07 +0200

    Merge dev into master (#45)
    * Stop the ticker when calling stop on SendRepeater
    
    * replaced GGQ with RingBuffer implementation

---
+++ examples/ttt/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
+func main() {
+	e := actor.NewEngine()
+	pid := e.SpawnFunc(func(c *actor.Context) {
+		switch msg := c.Message().(type) {
+		case actor.Started:
+			fmt.Println("started")
+		case actor.Stopped:
+			fmt.Println("stopped")
+		default:
+			_ = msg
+		}
+	}, "foobarbas")
+
+	wg := sync.WaitGroup{}
+	e.Poison(pid, &wg)
+	wg.Wait()
+	time.Sleep(time.Second * 2)
+}
