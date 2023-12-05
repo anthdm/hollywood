b6373b0b98304a3822d70f2112b078dab1e95f98
AR1011 <77502086+AR1011@users.noreply.github.com>
2023-12-05 18:25:15 +0000

    add trade-engine example (#82)
    * add trade-engine example
    
    * fix race condition
    
    * engine, err
    
    * remove go routines
    
    * remove active flag
    
    * switch to subscribe
    
    * stop using eventstream
    
    * fix comment
    
    * cleanup and readme
    
    * switch from slice to map
    
    * Replace Unix timestamp with time.Time in trade engine
    
    The commit updates the trade engine by replacing Unix timestamp format with Go's native time.Time. This affects order expiry and price update time tracking in the system. It's a better practice for readability and consistency in Go, and will also handle different time zones effectively. A logging line deemed excessively verbose was also commented out for cleanliness.
    
    * remove signal
    
    * enchance main.go
    
    * shut down the trade engine before exit.
    
    * remove depreciated log handler
    
    ---------
    
    Co-authored-by: Per Buer <per.buer@gmail.com>

---
+++ examples/trade-engine/actors/executor/executor.go
@@ -0,0 +1,137 @@
+package executor
+
+import (
+	"log/slog"
+	"time"
+
+	"github.com/anthdm/hollywood/actor"
+	"github.com/anthdm/hollywood/examples/trade-engine/types"
+)
+
+// Options for creating a new executor
+type ExecutorOptions struct {
+	PriceWatcherPID *actor.PID
+	TradeID         string
+	Ticker          string
+	Token0          string
+	Token1          string
+	Chain           string
+	Wallet          string
+	Pk              string
+	Expires         time.Time
+}
+
+type tradeExecutorActor struct {
+	id              string
+	ActorEngine     *actor.Engine
+	PID             *actor.PID
+	priceWatcherPID *actor.PID
+	ticker          string
+	token0          string
+	token1          string
+	chain           string
+	wallet          string
+	pk              string
+	status          string
+	lastPrice       float64
+	expires         time.Time
+}
+
+func (te *tradeExecutorActor) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Started:
+		slog.Info("tradeExecutor.Started", "id", te.id, "wallet", te.wallet)
+
+		// set actorEngine and PID
+		te.ActorEngine = c.Engine()
+		te.PID = c.PID()
+
+		// subscribe to price updates
+		te.ActorEngine.Send(te.priceWatcherPID, types.Subscribe{Sendto: te.PID})
+
+	case actor.Stopped:
+		slog.Info("tradeExecutor.Stopped", "id", te.id, "wallet", te.wallet)
+
+	case types.PriceUpdate:
+		// update the price
+		te.processUpdate(msg)
+
+	case types.TradeInfoRequest:
+		slog.Info("tradeExecutor.TradeInfoRequest", "id", te.id, "wallet", te.wallet)
+
+		// handle the request
+		te.handleTradeInfoRequest(c)
+
+	case types.CancelOrderRequest:
+		slog.Info("tradeExecutor.CancelOrderRequest", "id", te.id, "wallet", te.wallet)
+
+		// update status
+		te.status = "cancelled"
+
+		// stop the executor
+		te.Finished()
+	}
+}
+
+func (te *tradeExecutorActor) processUpdate(update types.PriceUpdate) {
+
+	// if expires is set and is less than current time, cancel the order
+	if !te.expires.IsZero() && time.Now().After(te.expires) {
+		slog.Info("Trade Expired", "id", te.id, "wallet", te.wallet)
+		te.Finished()
+		return
+	}
+
+	// update the price
+	te.lastPrice = update.Price
+
+	// do something with the price
+	// eg update pnl, etc
+
+	// for example just print price
+	// XXX a bit too much noise.
+	// slog.Info("tradeExecutor.PriceUpdate", "ticker", update.Ticker, "price", update.Price)
+}
+
+func (te *tradeExecutorActor) handleTradeInfoRequest(c *actor.Context) {
+	c.Respond(types.TradeInfoResponse{
+		// for example
+		Foo:   100,
+		Bar:   100,
+		Price: te.lastPrice,
+	})
+}
+
+func (te *tradeExecutorActor) Finished() {
+	// make sure ActorEngine and PID are set
+	if te.ActorEngine == nil {
+		slog.Error("tradeExecutor.actorEngine is <nil>")
+	}
+
+	if te.PID == nil {
+		slog.Error("tradeExecutor.PID is <nil>")
+	}
+
+	// unsubscribe from price updates
+	te.ActorEngine.Send(te.priceWatcherPID, types.Unsubscribe{Sendto: te.PID})
+
+	// poision itself
+	te.ActorEngine.Poison(te.PID)
+}
+
+func NewExecutorActor(opts *ExecutorOptions) actor.Producer {
+	return func() actor.Receiver {
+		return &tradeExecutorActor{
+			id:              opts.TradeID,
+			ticker:          opts.Ticker,
+			token0:          opts.Token0,
+			token1:          opts.Token1,
+			chain:           opts.Chain,
+			wallet:          opts.Wallet,
+			pk:              opts.Pk,
+			priceWatcherPID: opts.PriceWatcherPID,
+			expires:         opts.Expires,
+			status:          "active",
+		}
+	}
+}
---
+++ examples/trade-engine/types/types.go
@@ -0,0 +1,64 @@
+package types
+
+import (
+	"github.com/anthdm/hollywood/actor"
+	"time"
+)
+
+// message to cancel a trade
+type CancelOrderRequest struct {
+	TradeID string
+}
+
+// message sent to get trade info
+type TradeInfoRequest struct {
+	TradeID string
+}
+
+// response message for trade info
+type TradeInfoResponse struct {
+	// info regarding the current position
+	// eg price, pnl, etc
+	Foo   int
+	Bar   int
+	Price float64 // using float in example
+}
+
+// message sent to create a new trade order
+type TradeOrderRequest struct {
+	TradeID    string
+	Token0     string
+	Token1     string
+	Chain      string
+	Wallet     string
+	PrivateKey string
+	Expires    time.Time
+}
+
+// options when creating new price watcher
+type PriceOptions struct {
+	Ticker string
+	Token0 string
+	Token1 string
+	Chain  string
+}
+
+// price update from price watcher
+type PriceUpdate struct {
+	Ticker    string
+	UpdatedAt time.Time
+	Price     float64
+}
+
+// subscribe to price watcher
+type Subscribe struct {
+	Sendto *actor.PID
+}
+
+// unsubscribe from price watcher
+type Unsubscribe struct {
+	Sendto *actor.PID
+}
+
+// used with SendRepeat to trigger price update
+type TriggerPriceUpdate struct{}
