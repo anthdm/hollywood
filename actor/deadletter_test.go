ba84b41c5f975f7c408cfcf810ac66f3d51a701f
Per Buer <per.buer@gmail.com>
2023-12-06 18:57:04 +0100

    we cannot test the default deadletter handling as there is no handling of it in the default.

--- actor/deadletter_test.go
+++ actor/deadletter_test.go
@@ -3,26 +3,9 @@ package actor
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
-	"log/slog"
 	"sync"
 	"testing"
-	"time"
 )
-
-// TestDeadLetterDefault tests the default deadletter handling.
-// It will spawn a new actor, kill it, send a message to it and then check if there is a message
-// logged to the default logger
-func TestDeadLetterDefault(t *testing.T) {
-	logBuffer := SafeBuffer{}
-	logger := slog.New(slog.NewTextHandler(&logBuffer, &slog.HandlerOptions{Level: slog.LevelDebug}))
-	slog.SetDefault(logger)
-	e, err := NewEngine()
-	assert.NoError(t, err)
-	e.Send(invalidPid(), "bar")  // should end up the deadletter queue
-	time.Sleep(time.Millisecond) // wait for the deadletter to be processed
-	// check the log buffer for the deadletter
-	assert.Contains(t, logBuffer.String(), "deadletter arrived")
-}
 
 // TestDeadLetterCustom tests the custom deadletter handling.
 // It is using the custom deadletter receiver defined inline.
