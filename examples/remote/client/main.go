cb3c99f866ef970fbbaa102a92595609586500a3
Per Buer <per.buer@gmail.com>
2023-12-20 10:20:13 +0100

    more debug logs for the remote stuff.

--- examples/remote/client/main.go
+++ examples/remote/client/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log/slog"
+	"os"
 	"time"
 
 	"github.com/anthdm/hollywood/actor"
@@ -9,6 +11,7 @@ import (
 )
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug})))
 	r := remote.New("127.0.0.1:3000", nil)
 	e, err := actor.NewEngine(&actor.EngineOpts{Remote: r})
 	if err != nil {
@@ -18,6 +21,7 @@ func main() {
 	pid := actor.NewPID("127.0.0.1:4000", "server")
 	for {
 		e.Send(pid, &msg.Message{Data: "hello!"})
+		slog.Debug("sent message", "to", pid.String())
 		time.Sleep(time.Second)
 	}
 }
