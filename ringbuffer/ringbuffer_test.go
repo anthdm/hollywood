a4d658ebc605aa7aa50d9cd85be926067bf402ca
mapogolions <ivanovpavelalex45@gmail.com>
2025-01-06 16:01:01 +0500

    reproduce race condition

--- ringbuffer/ringbuffer_test.go
+++ ringbuffer/ringbuffer_test.go
@@ -1,6 +1,8 @@
 package ringbuffer
 
 import (
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
@@ -36,4 +38,58 @@ func TestPushPopN(t *testing.T) {
 			t.Fatal("invalid item popped")
 		}
 	}
+}
+
+func TestPopThreadSafety(t *testing.T) {
+	t.Run("Pop should be thread-safe", func(t *testing.T) {
+		testCase := func() {
+			rb := New[int](4)
+			rb.Push(1)
+			wg := sync.WaitGroup{}
+			for i := 0; i < 2; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					rb.Pop()
+				}()
+			}
+			wg.Wait()
+			if rb.Len() == -1 {
+				t.Fatal("item popped twice")
+			}
+		}
+
+		// Increase the number of iterations to raise the likelihood of reproducing the race condition
+		for i := 0; i < 100_000; i++ {
+			testCase()
+		}
+	})
+
+	t.Run("PopN should be thread-safe", func(t *testing.T) {
+		testCase := func() {
+			rb := New[int](4)
+			rb.Push(1)
+			counter := atomic.Int32{}
+			wg := sync.WaitGroup{}
+			for i := 0; i < 2; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					_, ok := rb.PopN(1)
+					if ok {
+						counter.Add(1)
+					}
+				}()
+			}
+			wg.Wait()
+			if counter.Load() > 1 {
+				t.Fatal("false positive item removal")
+			}
+		}
+
+		// Increase the number of iterations to raise the likelihood of reproducing the race condition
+		for i := 0; i < 100_000; i++ {
+			testCase()
+		}
+	})
 }
